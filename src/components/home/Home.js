5cf4f2152f44dc8f455f69fd44510975539aeb42
Ravi Kumar Ray <121496417+Ravilovecode@users.noreply.github.com>
2024-07-15 14:13:16 +0530

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import BannerImage from "./home-banner-image.png";
-import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+
 
 
 const Home = () => {
